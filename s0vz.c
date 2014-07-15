7703c3414b9360ab26d8e30005016af67daef518
slayerrensky <rensky.g@googlemail.com>
2014-07-15 23:51:54 +0200

    mit zeit

--- s0vz.c
+++ s0vz.c
@@ -271,7 +271,7 @@ void update_average_values(struct valuePack *vP) {
         tmp_value = wattProImpuls * (3.6 / (double)time) * 1000000.0; // Zeit in MS
 	    vP->valuesAsSumm += tmp_value / 1000.0;
 	    vP->numberOfValues++;
-	    printf("Summe: %.3f Anzahl %d TMPValue: %.3f Watt/imp: %f \n", vP->valuesAsSumm, vP->numberOfValues, tmp_value, wattProImpuls );
+	    printf("Summe: %.3f Anzahl %d TMPValue: %.3f Zeit: %d ms \n", vP->valuesAsSumm, vP->numberOfValues, tmp_value, time );
 	}
 	else
 		vP->lastTs = ts;
