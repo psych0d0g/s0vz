e8509cfdaa916a670aea109cfd9feaf3dbb21904
slayerrensky <rensky.g@googlemail.com>
2014-07-15 23:52:33 +0200

    ts angepasst

--- s0vz.c
+++ s0vz.c
@@ -266,7 +266,7 @@ void update_average_values(struct valuePack *vP) {
 	double tmp_value = 0;
 	if (vP->lastTs != 0)
 	{
-		time = (int)ts-vP->lastTs;
+		time = (int)(ts-vP->lastTs);
 		wattProImpuls = 1000.0 / (double)vP->impulsConst;
         tmp_value = wattProImpuls * (3.6 / (double)time) * 1000000.0; // Zeit in MS
 	    vP->valuesAsSumm += tmp_value / 1000.0;
